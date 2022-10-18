af3bf9af627c6324f23fc3ffecda23ff1201b5a3
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-10-18 02:38:29 -0300

    adding new fbp

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,35 @@
+import React from 'react';
+import Router from 'next/router';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
 
+function FacebookPixel() {
+  React.useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1031129740637357');
+        ReactPixel.pageView();
+
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+  return null;
+}
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Toaster position="bottom-right" />
-      <Component {...pageProps} />    
-    </ThemeProvider>
+    <>
+      <FacebookPixel />
+      <ThemeProvider theme={theme}>
+        <Toaster position="bottom-right" />
+        <Component {...pageProps} />    
+      </ThemeProvider>
+    </>
   )
 }
 
